81347983b92af15bf98c59a9a707a4d2516d52fb
Argus817 <namantandel@gmail.com>
2024-06-19 14:05:34 +0530

    Update solve.py

--- WEEK2/CryptoHack/Symmetric Cryptography/08. Modes of Operation Starter/solve.py
+++ WEEK2/CryptoHack/Symmetric Cryptography/08. Modes of Operation Starter/solve.py
@@ -1,10 +1,10 @@
 import requests
-import hashlib
+from json import loads
 from binascii import hexlify,unhexlify
 
 url = "https://aes.cryptohack.org/block_cipher_starter/"
 
-encrypt_flag = lambda : unhexlify(eval(requests.get(url+"encrypt_flag/").text)["ciphertext"].encode())
-decrypt = lambda ciphertext, password_hash : unhexlify(eval(requests.get(url+"decrypt/"+hexlify(ciphertext).decode()+'/'+hexlify(password_hash).decode()+'/').text)["plaintext"].encode())
+encrypt_flag = lambda : unhexlify(loads(requests.get(url+'encrypt_flag/').text)["ciphertext"].encode())
+decrypt = lambda ciphertext : unhexlify(loads(requests.get(f"{url}decrypt/{hexlify(ciphertext).decode()}/").text)['plaintext'].encode())
 
-print(decrypt(encrypt_flag(), hashlib.md5("keyword".encode()).digest()))
+print(decrypt(encrypt_flag()))
